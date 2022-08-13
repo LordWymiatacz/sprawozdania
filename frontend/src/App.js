5efa73cad3cb3c502cd467537bf5ab5953876a41
cirnet <78275621+cirnet@users.noreply.github.com>
2022-08-13 22:31:56 +0200

    app.js with routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Tasks from "./Tasks";
+import Task from "./Task";
+import Header from "./Header";
+import Home from "./Home";
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Header/>
+      
+      <div className='app'>
+
+      
+      
+      <Routes>
+        <Route path="/" element={<Tasks />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/tasks/:id" element={<Task/>} />
+      </Routes>
+      
+      </div>
+  </BrowserRouter>
+  )
 }
 
 export default App;
