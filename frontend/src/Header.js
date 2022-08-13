b8c9ec2d7c633740964ab5e2150fafc35ee7b7b2
cirnet <78275621+cirnet@users.noreply.github.com>
2022-08-13 22:23:33 +0200

    mod link nad correct href

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,34 +1,50 @@
+import './TaskCard.css'
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
-function Header() {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
 
-export default Header;
+export default function Header(){
+
+    return (
+        <>
+        <Navbar expand="lg" style={{ borderBottom:`1px solid blue`}}>
+          <Container>
+            <Navbar.Brand as={Link} to='/'>Sprawozdania z zajęć</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse className="justify-content-end">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to='/home'>Home</Nav.Link>
+                <Nav.Link as={Link} to='/about'>O systemie</Nav.Link>
+                <NavDropdown title="Grupy" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Dodaj Grupe</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to='/users'>Sprawdz wyniki</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Przyszłe funkcje
+                  </NavDropdown.Item>
+                  
+                </NavDropdown>
+              </Nav>
+              <Navbar.Text>
+            Zalogowany jako: <a href="#login">Mark Otto</a>
+          </Navbar.Text>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      
+      </>
+      
+      );
+    }    
