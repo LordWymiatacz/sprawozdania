999068b43f33ef5f0aaf211377d5b2067a512500
cirnet <78275621+cirnet@users.noreply.github.com>
2022-08-14 22:32:52 +0200

    deleted reportWebVitals

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,12 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-
+import About from './About'
 import Tasks from "./Tasks";
 import Task from "./Task";
 import Header from "./Header";
 import Home from "./Home";
-
+import CheckUser from "./User/CheckUser";
 
 
 function App() {
@@ -24,7 +24,9 @@ function App() {
       
       <Routes>
         <Route path="/" element={<Tasks />} />
+        <Route path="/users" element={<CheckUser/> } />
         <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About/>} />
         <Route path="/tasks/:id" element={<Task/>} />
       </Routes>
       
