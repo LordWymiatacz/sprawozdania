4f8090862a869c5519ff0bffb26bbc40e11f1338
cirnet <78275621+cirnet@users.noreply.github.com>
2022-08-14 22:22:17 +0200

    About section

---
+++ frontend/src/About.js
@@ -0,0 +1,14 @@
+export default function About(){
+    return(
+        <div>
+            <h1>Sprawozdania z zajęć</h1>
+
+
+            <p>Integer eget arcu at velit auctor mollis ac eu nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce velit dolor, ullamcorper sed erat et, posuere mollis libero. Maecenas et odio vitae arcu tempus sollicitudin vitae eget mauris. Quisque luctus rhoncus pulvinar. Integer sagittis tincidunt efficitur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc turpis leo, sodales in eleifend quis, cursus ac risus. Phasellus lacinia lacinia tellus at hendrerit. Praesent eleifend, mauris eu euismod dictum, lectus purus bibendum enim, vehicula condimentum erat mi ornare nibh. Ut lacinia consectetur mauris a consectetur. Nam sem mauris, ultrices et dignissim at, sodales quis neque. Ut in odio id velit viverra facilisis. Cras non libero arcu. Sed sed mi vehicula, imperdiet enim sit amet, molestie metus.</p>
+
+            
+
+            <p> facilisis convallis felis sit amet dignissim. Curabitur lacinia consectetur sollicitudin. Donec pellentesque, quam nec hendrerit porttitor, augue mauris lobortis ipsum, vel semper lorem ex vitae dolor. Donec erat nunc, blandit eget suscipit in, rutrum vitae enim. Proin lacus purus, porta eget odio eu, placerat mollis augue. Sed rhoncus ante ac purus malesuada, non blandit arcu pulvinar. Suspendisse posuere lacus massa, at porta ligula lacinia quis. In quis ipsum placerat, luctus leo vel, ornare eros. Curabitur a justo velit. Fusce mauris risus, mollis id condimentum vitae, ultricies hendrerit lectus. In sapien lorem, semper quis neque non, tristique tincidunt tellus.</p>
+        </div>
+    )
+}
