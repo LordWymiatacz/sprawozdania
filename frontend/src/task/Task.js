a9b62d8842b1049535e2d5482125c61160ff7adb
cirnet <78275621+cirnet@users.noreply.github.com>
2022-08-21 14:28:52 +0200

    added checkuser with params to task

--- frontend/src/task/Task.js
+++ frontend/src/task/Task.js
@@ -5,7 +5,7 @@ import { CircularProgress } from '@mui/material';
 import './TaskCard.css'
 import {useParams} from "react-router-dom";
 import UploadFile from "../upload/UploadFile";
-
+import ContentModalUserScore from '../ContentModalUserScore'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import CheckUser from "../User/CheckUser";
@@ -26,23 +26,25 @@ import CheckUser from "../User/CheckUser";
     <div className="content">
     {content? (
     <div>
+
         <h1> {content.title? `${content.title}`:" Brak nazwy zadania"}</h1>
         <ReactMarkdown children={content.description? content.description: " Brak opisu"} remarkPlugins={[remarkGfm]} />
         {/* <p><b><i>Prowadzący:</i></b> {content.username? `${content.username}`:" _______________"}</p> */}
         
+        <div className="userResults">
+        {/* Grupa 1 */}
+        <CheckUser  id_task={content.id} group_task='1'/>
+        {/* Grupa 2 */}
+        <CheckUser  id_task={content.id} group_task='2'/>
+
+
+        </div>
+      
          </div>
 
-        ):<CircularProgress className="loader"/>}
+        ):<CircularProgress className="loader"/>
 
-        
-        
-      {/* <UploadFile/> */}
-
-<CheckUser/>
-
-        <div>Grupa 1</div>
-        <div>Grupa 2</div>
-        <div>Grupa 3</div>
+        }
 
         </div>)
 }
