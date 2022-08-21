32f01e11412490681652218cd16df06a2c4d2df9
cirnet <78275621+cirnet@users.noreply.github.com>
2022-08-21 16:19:43 +0200

    deleted useless links

--- frontend/src/header/Header.js
+++ frontend/src/header/Header.js
@@ -24,20 +24,8 @@ export default function Header(){
               <Nav className="me-auto">
                 <Nav.Link as={Link} to='/home'>Home</Nav.Link>
                 <Nav.Link as={Link} to='/about'>O systemie</Nav.Link>
-                <NavDropdown title="Grupy" id="basic-nav-dropdown">
-                  <NavDropdown.Item as={Link} to='/addGroups'>Dodaj Grupe</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to='/addTasks'>Dodaj Task</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to='/users'>Sprawdz wyniki</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Przyszłe funkcje
-                  </NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link as={Link} to='/addGroups'>Dodaj Grupe</Nav.Link>
                 <Nav.Link as={Link} to='/groups'>Wyswietl grupy</Nav.Link>
-                <Nav.Link as={Link} to='/tasks'>Wyswietl taski</Nav.Link>
               </Nav>
               <Navbar.Text>
             Zalogowany jako: <a href="#login">Mark Otto</a>
