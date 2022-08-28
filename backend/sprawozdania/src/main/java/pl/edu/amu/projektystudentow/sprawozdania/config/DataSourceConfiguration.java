067788fb7dd495bf7842513c64b458d3ea51d5a4
LordWymiatacz <tomasz.wasylyk@gmail.com>
2022-08-28 16:44:47 +0200

    hange datasource config for heroku deployment

--- backend/sprawozdania/src/main/java/pl/edu/amu/projektystudentow/sprawozdania/config/DataSourceConfiguration.java
+++ backend/sprawozdania/src/main/java/pl/edu/amu/projektystudentow/sprawozdania/config/DataSourceConfiguration.java
@@ -1,5 +1,8 @@
 package pl.edu.amu.projektystudentow.sprawozdania.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,11 +13,17 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfiguration {
 
-    @Bean
-    @ConfigurationProperties("spring.datasource")
-    public DataSource getDataSource() {
-        return DataSourceBuilder.create().build();
+    @Configuration
+    public class DatabaseConfig {
 
+        @Value("${spring.datasource.url}")
+        private String dbUrl;
+
+        @Bean
+        public DataSource dataSource() {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
     }
-
 }
