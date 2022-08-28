cfe8fb3b28af2ef26ca7673adbd2bfdcef805135
LordWymiatacz <tomasz.wasylyk@gmail.com>
2022-08-28 19:11:27 +0200

    move backend to another repo

--- backend/sprawozdania/src/main/java/pl/edu/amu/projektystudentow/sprawozdania/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.amu.projektystudentow.sprawozdania.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.edu.amu.projektystudentow.sprawozdania.entity.Student;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
